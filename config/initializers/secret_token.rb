d8ff2287105a65fe3b902a32f840cf7e2d18731f
Scrulox <git config --global user.email hk.ling09@yahoo.comgit config --global user.email>
2014-07-17 14:44:38 -0700

    Started treehouse with status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '3f4e964449b798c3bcdf844c9063d1a67a6504f280c0f93d902f5300c8c8eb4bfc098bc294893311d0b10fff857c1486c6644d2fe1328bacb550d740994ca3a2'
